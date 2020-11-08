402b0da42a21e4604bb6c93f01a26993ed9bde94
Reavus3 <andrewconstant@hotmail.com>
2020-11-08 14:06:43 +0000

    added loop to publish all posts to index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -33,6 +34,8 @@ class PostsController < ApplicationController
   end
   
   def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
